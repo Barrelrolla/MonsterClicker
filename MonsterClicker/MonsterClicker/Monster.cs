d16fdd2c068d950550d8d829bc332160d56387d4
Barrelrolla <chetkara@gmail.com>
2016-06-26 17:30:11 +0300

    Fixed an error in achievements which made it crash.

--- MonsterClicker/MonsterClicker/Monster.cs
+++ MonsterClicker/MonsterClicker/Monster.cs
@@ -8,20 +8,11 @@
         private BigInteger nextLevelHealth = StartHealth;
         private BigInteger killCount = 1;
 
-        public delegate void AchievementHandler(object sender, AchievementArgs a);
-
-        public event AchievementHandler OnKillsReached;
-
         public override void GenerateHealth()
         {
             this.nextLevelHealth += this.nextLevelHealth / 7;
             this.Health = this.nextLevelHealth;
             this.killCount++;
-            if (this.killCount == 100)
-            {
-                AchievementArgs killsAchievemnt = new AchievementArgs("Killed 100 monsters!");
-                this.OnKillsReached(this, killsAchievemnt);
-            }
         }
 
         public override void GenerateInventory()
